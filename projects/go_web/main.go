8e831d44cc3f5fde642eb272e946b439ca664566
Ivo Bosticky <ivo@adnuntius.com>
2024-06-09 13:54:38 +1000

    go lib and go web app

---
+++ projects/go_web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/ivobos/ivobos-bazel-monorepo/projects/go_hello_world"
+	"log"
+	"net/http"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request")
+	w.Write([]byte(go_hello_world.HelloWorld()))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", YourHandler)
+	log.Println("Going to listen to port 8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
